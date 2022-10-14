5633a043e701bc4ab78dd1632718d7b75c50e1cf
Ijerk7 <adubanture@yahoo.com>
2022-10-14 03:19:32 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,12 +4,12 @@
 /**
  * main - main block
  * Return: 1
- */ 
+ */
 int main(void)
 {
-int s;
-s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-s);
-return (1);
+	int s;
+	s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that iece of art is useful\" - Dora Korpar, 2015-10-19\n",
+			s);
+	return (1);
 }
