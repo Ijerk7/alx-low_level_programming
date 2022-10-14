65e9d24f7cea659a1c56c0ab0b6cb65e1ca60cf0
Ijerk7 <adubanture@yahoo.com>
2022-10-13 22:41:05 -0700

    printf .c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,18 +1,10 @@
-#!/bin/bash
 #include <stdio.h>
 
-
-
 /**
+ * main - Entry point
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
+ * Return: Always 0 (Success)
+ */
 int main(void)
 
 {
